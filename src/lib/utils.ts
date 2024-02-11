1a9a8cf84030685c6a255bd93bbcf1e5d5f1080f
Sourav <souravberaofficial@gmail.com>
2024-02-11 20:20:47 +0530

    update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -18,7 +18,7 @@ export const formatPrice = (price: Price) => {
 export const getURL = () => {
   let url =
     process?.env?.NEXT_PUBLIC_SITE_URL ??
-    'http://localhost:3000/';
+    'https://co-lab-ai.vercel.app/';
 
   url = url.includes('http') ? url : `https://${url}`;
   url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
