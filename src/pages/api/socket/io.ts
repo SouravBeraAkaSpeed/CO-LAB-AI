04b46467a8a4873dc26cf5aa318ced8886e1e32b
Sourav <souravberaofficial@gmail.com>
2024-02-11 21:13:01 +0530

    update

--- src/pages/api/socket/io.ts
+++ src/pages/api/socket/io.ts
@@ -10,12 +10,14 @@ export const config = {
 };
 
 const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
+  
   if (!res.socket.server.io) {
     const path = "/api/socket/io";
     const httpServer: NetServer = res.socket.server as any;
     const io = new ServerIO(httpServer, {
       path,
       addTrailingSlash: false,
+      cors: { origin: req.headers.origin || "*", methods: ["GET", "POST"] },
     });
     io.on("connection", (s) => {
       s.on("create-room", (fileId) => {
